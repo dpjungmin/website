d933ead864e847034249104cf343d9adc10029dd
dpjungmin <dpjungmin@gmail.com>
2021-07-12 18:04:07 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "website",
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-gatsby-cloud"],
+};
