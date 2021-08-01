7376bff7f60b3960bb6ba774ffdf0e59ab317063
dpjungmin <dpjungmin@gmail.com>
2021-08-01 06:36:45 -0700

    minor update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,13 @@ const IndexPage: React.FC<IndexPageProps> = ({ location }) => {
       <SEO />
       <$.Container>
         <h1>Hello!</h1>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.feedspot.com/infiniterss.php?q=site:https://dpjungmin.gatsbyjs.io/rss.xml"
+        >
+          Follow RSS
+        </a>
       </$.Container>
     </Layout>
   )
--- src/styles/pages/index.styles.ts
+++ src/styles/pages/index.styles.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
   min-height: 100vh;
 `
